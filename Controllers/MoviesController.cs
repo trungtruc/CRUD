ee415ad21ca710fd5505cdd44a331940f2ea1720
trungtruc <trungtruc.uit@gmail.com>
2020-04-15 08:53:35 +0700

    test

---
+++ Controllers/MoviesController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CRUD.Models;
+
+namespace CRUD.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        public ActionResult Random()
+        {
+            var movie = new Movie() { Name = "Shrek!" };
+            return View(movie);
+        }
+
+        /*public ActionResult Edit(int id)
+        {
+            return Content($"id={id}");
+        }*/
+    }
+}
